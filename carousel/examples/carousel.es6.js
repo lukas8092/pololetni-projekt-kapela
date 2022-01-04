3e724946bb9db16accb11a4df908681c8b35ce42
Lukáš Štěp <step@SPSEJECNANET.CZ>
2022-01-04 11:04:06 +0100

    concerts, albums

--- carousel/examples/carousel.es6.js
+++
@@ -1,9 +0,0 @@
-import bootstrap from 'bootstrap';
-import $ from 'jquery';
-
-import Carousel from '../src/bootstrap-swipe-carousel';
-
-$(() => {
-  $('.js-carousel').carousel().swipeCarousel();
-});
-
